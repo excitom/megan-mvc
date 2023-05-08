f4244b19cdd497593afb78bd8182478e26dfd980
excitom <tlang@halsoft.com>
2023-05-08 19:22:56 +0000

    add DB credentials

--- .secrets.php
+++ .secrets.php
@@ -1,2 +1,6 @@
 <?php
-$_SERVER['FOO'] = "BAR";
+$_SERVER['DB_HOST'] = "localhost";
+$_SERVER['DB_DEFAULT'] = "megan";
+$_SERVER['DB_USER'] = "root";
+$_SERVER['DB_PASSWORD'] = "Sesame";
+$_SERVER['ENCRYPTION_KEY'] = "alchiga";
