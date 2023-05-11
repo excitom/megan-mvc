dab0215e2d05ad9f4e388ff4c70dcd6901107d0a
excitom <tlang@halsoft.com>
2023-05-11 20:32:14 +0000

    template for the .secrets.php file

---
+++ secrets-template.php
@@ -0,0 +1,14 @@
+// fill in the following variables and save this file as 
+// .secrets.php in the same directory as fw.php.
+// Refer to the PHP openssl_encrypt man page for description of the 
+// encryption keys, pepper, cypher, etc.
+<?php
+$_SERVER['DB_HOST'] =
+$_SERVER['DB_DEFAULT'] =
+$_SERVER['DB_USER'] =
+$_SERVER['DB_PASSWORD'] =
+$_SERVER['HASH_KEY'] =
+$_SERVER['PEPPER'] =
+$_SERVER['ENCRYPTION_KEY_1'] =
+$_SERVER['ENCRYPTION_KEY_2'] =
+$_SERVER['CIPHER'] =
